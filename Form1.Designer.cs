f0399f9eb9e6e6435574d265bfd792fd2b8883a4
02Naitsirk <iifrostii@outlook.com>
2022-04-03 23:19:06 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace TwitchPredictionsGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberOfPointsLabel = new System.Windows.Forms.Label();
+            this.choiceALabel = new System.Windows.Forms.Label();
+            this.choiceBLabel = new System.Windows.Forms.Label();
+            this.aSuccessProbLabel = new System.Windows.Forms.Label();
+            this.goButton = new System.Windows.Forms.Button();
+            this.betLabel = new System.Windows.Forms.Label();
+            this.numberOfPointsInput = new System.Windows.Forms.NumericUpDown();
+            this.aPointsInput = new System.Windows.Forms.NumericUpDown();
+            this.bPointsInput = new System.Windows.Forms.NumericUpDown();
+            this.aProbInput = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfPointsInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aPointsInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bPointsInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aProbInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numberOfPointsLabel
+            // 
+            this.numberOfPointsLabel.Location = new System.Drawing.Point(12, 9);
+            this.numberOfPointsLabel.Name = "numberOfPointsLabel";
+            this.numberOfPointsLabel.Size = new System.Drawing.Size(100, 23);
+            this.numberOfPointsLabel.TabIndex = 0;
+            this.numberOfPointsLabel.Text = "Points You Have";
+            // 
+            // choiceALabel
+            // 
+            this.choiceALabel.Location = new System.Drawing.Point(12, 32);
+            this.choiceALabel.Name = "choiceALabel";
+            this.choiceALabel.Size = new System.Drawing.Size(100, 23);
+            this.choiceALabel.TabIndex = 1;
+            this.choiceALabel.Text = "Points on A";
+            // 
+            // choiceBLabel
+            // 
+            this.choiceBLabel.Location = new System.Drawing.Point(12, 55);
+            this.choiceBLabel.Name = "choiceBLabel";
+            this.choiceBLabel.Size = new System.Drawing.Size(100, 23);
+            this.choiceBLabel.TabIndex = 2;
+            this.choiceBLabel.Text = "Points on B";
+            // 
+            // aSuccessProbLabel
+            // 
+            this.aSuccessProbLabel.Location = new System.Drawing.Point(12, 78);
+            this.aSuccessProbLabel.Name = "aSuccessProbLabel";
+            this.aSuccessProbLabel.Size = new System.Drawing.Size(164, 23);
+            this.aSuccessProbLabel.TabIndex = 3;
+            this.aSuccessProbLabel.Text = "Choice A Success Probability (%)";
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(12, 102);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(270, 23);
+            this.goButton.TabIndex = 10;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // betLabel
+            // 
+            this.betLabel.Location = new System.Drawing.Point(12, 128);
+            this.betLabel.Name = "betLabel";
+            this.betLabel.Size = new System.Drawing.Size(270, 23);
+            this.betLabel.TabIndex = 11;
+            this.betLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numberOfPointsInput
+            // 
+            this.numberOfPointsInput.Location = new System.Drawing.Point(134, 7);
+            this.numberOfPointsInput.Maximum = new decimal(new int[] { 2147483647, 0, 0, 0 });
+            this.numberOfPointsInput.Name = "numberOfPointsInput";
+            this.numberOfPointsInput.Size = new System.Drawing.Size(148, 20);
+            this.numberOfPointsInput.TabIndex = 12;
+            // 
+            // aPointsInput
+            // 
+            this.aPointsInput.Location = new System.Drawing.Point(134, 30);
+            this.aPointsInput.Maximum = new decimal(new int[] { 2147483647, 0, 0, 0 });
+            this.aPointsInput.Name = "aPointsInput";
+            this.aPointsInput.Size = new System.Drawing.Size(148, 20);
+            this.aPointsInput.TabIndex = 13;
+            // 
+            // bPointsInput
+            // 
+            this.bPointsInput.Location = new System.Drawing.Point(134, 53);
+            this.bPointsInput.Maximum = new decimal(new int[] { 2147483647, 0, 0, 0 });
+            this.bPointsInput.Name = "bPointsInput";
+            this.bPointsInput.Size = new System.Drawing.Size(148, 20);
+            this.bPointsInput.TabIndex = 14;
+            // 
+            // aProbInput
+            // 
+            this.aProbInput.DecimalPlaces = 2;
+            this.aProbInput.Location = new System.Drawing.Point(182, 76);
+            this.aProbInput.Name = "aProbInput";
+            this.aProbInput.Size = new System.Drawing.Size(100, 20);
+            this.aProbInput.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(294, 159);
+            this.Controls.Add(this.aProbInput);
+            this.Controls.Add(this.bPointsInput);
+            this.Controls.Add(this.aPointsInput);
+            this.Controls.Add(this.numberOfPointsInput);
+            this.Controls.Add(this.betLabel);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.aSuccessProbLabel);
+            this.Controls.Add(this.choiceBLabel);
+            this.Controls.Add(this.choiceALabel);
+            this.Controls.Add(this.numberOfPointsLabel);
+            this.Name = "Form1";
+            this.Text = "Twitch Predictions Helper";
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfPointsInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aPointsInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bPointsInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aProbInput)).EndInit();
+            this.ResumeLayout(false);
+            this.MaximizeBox = false;
+        }
+
+        private System.Windows.Forms.NumericUpDown aProbInput;
+
+        private System.Windows.Forms.NumericUpDown bPointsInput;
+
+        private System.Windows.Forms.NumericUpDown aPointsInput;
+
+        private System.Windows.Forms.NumericUpDown numberOfPointsInput;
+
+        private System.Windows.Forms.Label betLabel;
+
+        private System.Windows.Forms.Button goButton;
+
+        private System.Windows.Forms.Label aSuccessProbLabel;
+
+        private System.Windows.Forms.Label choiceALabel;
+        private System.Windows.Forms.Label choiceBLabel;
+
+        private System.Windows.Forms.Label numberOfPointsLabel;
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwitchPredictionsGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            int points = int.Parse(numberOfPointsInput.Text);
+
+            var aPoints = (int)aPointsInput.Value;
+            var bPoints = (int)bPointsInput.Value;
+
+            double aProbability = (double)aProbInput.Value / 100.0;
+            double bProbability = 1 - aProbability;
+
+            int aBet = CalculateBet(points, aPoints, bPoints, aProbability);
+            int bBet = CalculateBet(points, bPoints, aPoints, bProbability);
+
+            if (aBet > bBet)
+            {
+                betLabel.Text = $@"Bet {aBet} on Choice A";
+            }
+            else if (bBet > aBet)
+            {
+                betLabel.Text = $@"Bet {bBet} on Choice B";
+            }
+            else
+            {
+                betLabel.Text = @"Don't bet";
+            }
+        }
+
+        /// <summary>
+        /// Calculates the user's bet size for choice A if the user has <paramref name="w"/> points,
+        /// there are <paramref name="a"/>  points on choice A,
+        /// there are <paramref name="b"/>  points on choice B,
+        /// and the success probability of choice A is <paramref name="p"/>.
+        /// </summary>
+        /// <param name="w">
+        /// User's points
+        /// </param>
+        /// <param name="a">
+        /// Points on choice A
+        /// </param>
+        /// <param name="b">
+        /// Points on choice B
+        /// </param>
+        /// <param name="p">
+        /// Success probability of choice B
+        /// </param>
+        /// <returns></returns>
+        private static int CalculateBet(long w, long a, long b, double p)
+        {
+            if (p - 1 == 0)
+            {
+                return (int)w;
+            }
+
+            double x = 1 / (2 * (p - 1) * w * (b + w));
+            double y = -Math.Sqrt(a * b * (a * b - 4 * a * p * p * w + 4 * a * p * w - 4 * b * p * p * w +
+                4 * b * p * w - 4 * p * p * w * w + 4 * p * w * w));
+            double z = a * b - 2 * a * p * w + 2 * a * w;
+            double betProportion = x * (y + z);
+            return (int)Math.Round(betProportion * w);
+        }
+    }
+}
